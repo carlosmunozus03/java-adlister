6f7dd35eeed5c884cebdaf4ea588bf8d7e0ff935
Carlos Munoz <carlos.munoz.us03@gmail.com>
2021-07-08 14:19:29 -0500

    complete exercise session and cookies

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -10,49 +10,25 @@ import java.io.IOException;
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (request.getSession().getAttribute("isAdmin") != null) {
-            response.sendRedirect("/secret-admin-page.jsp");
-            return;
-        }
-
         request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        boolean isAdmin = username.equals("admin") && password.equals("password");
-
+//below code: session is the variable that is grabbing the current session
         HttpSession session = request.getSession();
 
-        if (isAdmin) {
-            session.setAttribute("isAdmin", true);
-            response.sendRedirect("/secret-admin-page");
+        boolean validAttempt = username.equals("admin") && password.equals("password");
+
+        if (validAttempt) {
+//            below code: assigns the user parameter to the username variable, which is passed onto the jsp via the session
+//            only terminated when told to or when the browser is closed
+            session.setAttribute("user", username);
+            response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
         }
-
-
-//        // set the session object to a variable based on the HttpSession class.
-//        HttpSession session = request.getSession();
-//
-//        String darkMode = request.getParameter("color");
-//
-//        // set the dark theme attribute on the session so that it gets remembered every time we log in.
-//        session.setAttribute("darkMode", darkMode);
-//
-//        // on a different servlet (profile page).
-//        // with our getAttribute method, our return is always a generic Object type.
-//        // therefore we need to explicitly cast the object to the correct type.
-//        session.getAttribute("darkMode");
-//        String darkSelected = (String) session.getAttribute("darkMode");
-//
-//        // to remove an attribute
-//        session.removeAttribute("darkMode");
-//
-//        // destroys the session
-//        session.invalidate();
-
-
     }
 }
